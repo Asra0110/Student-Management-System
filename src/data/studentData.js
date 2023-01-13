8cc56d0e184da73a8df9f72406893a84ead55db7
Asra0110 <111082124+Asra0110@users.noreply.github.com>
2023-01-13 17:42:06 +0530

    Update studentData.js

--- src/data/studentData.js
+++ src/data/studentData.js
@@ -4,49 +4,49 @@ const studentData = [
         firstName: "Asra",
         lastName:"Ansari",
         email:"asra@gmail.com",
-        rollno:"11361A1203",
-        dob:"1995-10-01"
+        rollno:"1212",
+        dob:"2022-01-01"
     },
     {
         id : 2,
-        firstName: "Asra",
-        lastName:"Ansari",
-        email:"asra@gmail.com",
-        rollno:"11361A1203",
-        dob:"1995-10-01"
+        firstName: "Laxmi",
+        lastName:"Bhanuja",
+        email:"laxmi@gmail.com",
+        rollno:"1111",
+        dob:"2022-01-01"
     },
     {
         id : 3,
-        firstName: "Asra",
-        lastName:"Ansari",
-        email:"asra@gmail.com",
-        rollno:"11361A1203",
-        dob:"1995-10-01"
+        firstName: "Rupesh",
+        lastName:"Pandey",
+        email:"rpandey@gmail.com",
+        rollno:"1010",
+        dob:"2022-01-01"
     },
     
     {
         id : 4,
-        firstName: "Asra",
-        lastName:"Ansari",
-        email:"asra@gmail.com",
-        rollno:"11361A1203",
-        dob:"1995-10-01"
+        firstName: "Yuraj",
+        lastName:"Singh",
+        email:"yuvi@gmail.com",
+        rollno:"1313",
+        dob:"2022-01-01"
     },
     {
         id : 5,
-        firstName: "Asra",
-        lastName:"Ansari",
-        email:"asra@gmail.com",
-        rollno:"11361A1203",
-        dob:"1995-10-01"
+        firstName: "Rajesh",
+        lastName:"Chaudary",
+        email:"rajesh@gmail.com",
+        rollno:"0909",
+        dob:"2022-01-01"
     },
     {
         id : 6,
-        firstName: "Asra",
-        lastName:"Ansari",
-        email:"asra@gmail.com",
-        rollno:"11361A1203",
-        dob:"1995-10-01"
+        firstName: "Deepak",
+        lastName:"Singh",
+        email:"deepak@gmail.com",
+        rollno:"0410",
+        dob:"2022-01-01"
     },
 
 ];
